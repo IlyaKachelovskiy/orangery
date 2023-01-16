9456440e44925529c76f6ddfd9577ead0fbeb6a4
Aleksandr Bondarenko <aleksandr.bondarenko@devexperts.com>
2023-01-16 17:18:26 +0400

    003: добавил в навигацию новый экран, удалил старый роут

--- lib/core/navigation/navigation_screen_factory.dart
+++ lib/core/navigation/navigation_screen_factory.dart
@@ -1,5 +1,8 @@
 import 'package:orangery/presentation/widgets/main_tabs/main_tabs_screen.dart';
+import 'package:orangery/presentation/widgets/ready_made_bouquets/ready_made_bouquets.dart';
 
 class NavigationScreenFactory {
   makeMainTabsScreen() => const MainTabsScreen();
+
+  makeReadyMadeBouquetsScreen() => const ReadyMadeBouquetsScreen();
 }
