76d2bee931ada5eac4349dd067d68a79b653bc46
Ilyakok <ilya.kachelovskiy@gmail.com>
2023-03-20 20:10:44 +0300

    005: add constants

--- lib/core/constant/ui_kit_colors.dart
+++ lib/core/constant/ui_kit_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class UiKitColors {
   static const white = Color(0xffFFFFFF);
+  static const black = Color(0xff000000);
+  static const grey = Color(0xaef6f6f6);
 }
